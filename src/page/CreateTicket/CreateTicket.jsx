ae4360e09b0956f72348e1d65865afce681c4906
Sebastian-123-web <sebastian09_26@hotmail.com>
2023-06-27 00:58:04 -0500

    RB | Se agrego del formulario el paso 2 completo y se avanzo el paso 3 solo los elementos que iran

--- src/page/CreateTicket/CreateTicket.jsx
+++ src/page/CreateTicket/CreateTicket.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ProgressBar from './ProgresoTicket';
+import '../../index.css'
 
 const CreateTicket = () => {
     const [step, setStep] = useState(1);
@@ -20,11 +21,11 @@ const CreateTicket = () => {
                     {/* Aquí va el contenido del primer paso */}
                     <h1 className='font-bold mb-5 text-xl'>Datos de Usuario</h1>
                     <p>Correo corporativo: <span className='text-red-600 font-bold'>*</span></p>
-                    <input type="text" className='mb-4 w-full rounded-md sm:text-sm mt-1 px-4 py-3 bg-[#FFF] border border-slate-300 focus:outline-none' placeholder='alguien@transberperu.com' />
+                    <input type="email" className='mb-4 w-full rounded-md sm:text-sm mt-1 px-4 py-3 bg-[#FFF] border border-slate-300 focus:outline-none' placeholder='alguien@transberperu.com' />
                     <p>Nombre y apellido: <span className='text-red-600 font-bold'>*</span></p>
                     <input type="text" className='mb-4 w-full rounded-md sm:text-sm mt-1 px-4 py-3 bg-[#FFF] border border-slate-300 focus:outline-none' placeholder='Juan Hutchcraft' />
                     <p>Telefono/Celular: <span className='text-red-600 font-bold'>*</span></p>
-                    <input type="text" className='mb-4 w-full rounded-md sm:text-sm mt-1 px-4 py-3 bg-[#FFF] border border-slate-300 focus:outline-none' placeholder='963741258' />
+                    <input type="number" className='mb-4 w-full rounded-md sm:text-sm mt-1 px-4 py-3 bg-[#FFF] border border-slate-300 focus:outline-none' placeholder='963741258' />
                     <p>Area: <span className='text-red-600 font-bold'>*</span></p>
                     <select name="" id="" className='mb-4 w-full rounded-md sm:text-sm mt-1 px-4 py-3 bg-[#FFF] border border-slate-300 focus:outline-none'>
                         <option select>Seleccione el area</option>
@@ -37,20 +38,35 @@ const CreateTicket = () => {
             {step === 2 && (
                 <div className='flex flex-col w-[425px]'>
                     {/* Aquí va el contenido del segundo paso */}
-                    <button onClick={handlePrevStep} className='inline-flex'>Anterior</button>
+                    <div className='mb-4 align-bottom'>
+                        <button onClick={handlePrevStep} className='text-[#9AAFC7] align-middle'><ion-icon name="chevron-back-outline"></ion-icon> Atras</button>
+                    </div>
                     <p>Descripcion breve: <span className='text-red-600 font-bold'>*</span></p>
                     <input type="text" className='mb-4 w-full rounded-md sm:text-sm mt-1 px-4 py-3 bg-[#FFF] border border-slate-300 focus:outline-none' placeholder='Ejemplo: No abre SIDIGE, Permisos SIDIGE' />
                     <p className='mb-3.5'>Seleccione el dispositivo que presenta inconveniete: <span className='text-red-600 font-bold'>*</span></p>
-                    <div className='flex justify-between mb-5'>
-                        <div className='w-16 h-16 border border-slate-300 bg-[#fff] rounded-lg flex justify-center items-center text-slate-300'><p className="text-3xl"><ion-icon name="desktop-outline"></ion-icon></p></div>
-                        <div className='w-16 h-16 border border-slate-300 bg-[#fff] rounded-lg flex justify-center items-center text-slate-300'><p className="text-3xl"><ion-icon name="laptop-outline"></ion-icon></p></div>
-                        <div className='w-16 h-16 border border-slate-300 bg-[#fff] rounded-lg flex justify-center items-center text-slate-300'><p className="text-3xl"><ion-icon name="phone-portrait-outline"></ion-icon></p></div>
-                        <div className='w-16 h-16 border border-slate-300 bg-[#fff] rounded-lg flex justify-center items-center text-slate-300'><p className="text-3xl"><ion-icon name="print-outline"></ion-icon></p></div>
+                    <div className='radioDevice flex justify-between mb-4'>
+                        <label>
+                            <input type="radio" name='device' value="desktop" className='none' />
+                            <div className={`w-16 h-16 border border-slate-300 bg-[#fff] rounded-lg flex justify-center items-center text-slate-300 hover:bg-[#270722b3] hover:text-[#fff] duration-300 cursor-pointer`} title="Computadora"><p className="text-3xl"><ion-icon name="desktop-outline"></ion-icon></p></div>
+                        </label>
+                        <label>
+                            <input type="radio" name='device' value="laptop" className='none' />
+                            <div className={`w-16 h-16 border border-slate-300 bg-[#fff] rounded-lg flex justify-center items-center text-slate-300 hover:bg-[#270722b3] hover:text-[#fff] duration-300 cursor-pointer`} title="Laptop"><p className="text-3xl"><ion-icon name="laptop-outline"></ion-icon></p></div>
+                        </label>
+                        <label>
+                            <input type="radio" name='device' value="movil" className='none' />
+                            <div className={`w-16 h-16 border border-slate-300 bg-[#fff] rounded-lg flex justify-center items-center text-slate-300 hover:bg-[#270722b3] hover:text-[#fff] duration-300 cursor-pointer`} title="Celular"><p className="text-3xl"><ion-icon name="phone-portrait-outline"></ion-icon></p></div>
+                        </label>
+                        <label>
+                            <input type="radio" name='device' value="impresora" className='none' />
+                            <div className={`w-16 h-16 border border-slate-300 bg-[#fff] rounded-lg flex justify-center items-center text-slate-300 hover:bg-[#270722b3] hover:text-[#fff] duration-300 cursor-pointer`} title="Impresora"><p className="text-3xl"><ion-icon name="print-outline"></ion-icon></p></div>
+                        </label>
                     </div>
-                    <div className='flex flex-col mb-5'>
-                        <input type="range" list='importancia' className='' />
+                    <p className='mb-3.5'>Nivel de Importancia: <span className='text-red-600 font-bold'>*</span></p>
+                    <div className='flex flex-col mb-4'>
+                        <input type="range" list='importancia' className='accent-[#270722]' />
                         <datalist id='importancia'>
-                            <option value="0" label='0%'>0%</option>
+                            <option value="0" label='0%'>0</option>
                             <option value="50" label='50%'>50%</option>
                             <option value="100" label='100%'>100%</option>
                         </datalist>
@@ -66,7 +82,21 @@ const CreateTicket = () => {
             {step === 3 && (
                 <div className='flex flex-col w-[425px]'>
                     {/* Aquí va el contenido del tercer paso */}
-                    <button onClick={handlePrevStep}>Anterior</button>
+                    <div className='mb-4'>
+                        <button onClick={handlePrevStep} className='text-[#9AAFC7]'><ion-icon name="chevron-back-outline"></ion-icon> Atras</button>
+                    </div>
+                    <div>
+                        <p>Detalle del problema:</p>
+                        <textarea name="" id="" cols="30" rows="5" className='mb-4 w-full rounded-md sm:text-sm mt-1 px-4 py-3 bg-[#FFF] border border-slate-300 focus:outline-none' placeholder='Especifique el problema'></textarea>
+                    </div>
+                    <div>
+                        <p>Codigo Anydesk: <span>(Opcional)</span></p>
+                        <input type="number" className='mb-4 w-full rounded-md sm:text-sm mt-1 px-4 py-3 bg-[#FFF] border border-slate-300 focus:outline-none' placeholder='1 321 987 654' />
+                    </div>
+                    <div>
+                        <p>Añadir una imagen: <span>(Opcional)</span></p>
+                        <input type="file" src="" alt="" />
+                    </div>
                     <button type="submit" className='w-full bg-[#270722] py-4 rounded-lg text-white font-bold'>Enviar</button>
                 </div>
             )}
--- src/page/CreateTicket/ProgresoTicket.jsx
+++ src/page/CreateTicket/ProgresoTicket.jsx
@@ -4,7 +4,7 @@ const ProgressBar = ({ currentStep, totalSteps }) => {
   const progress = ((currentStep - 1) / (totalSteps - 1)) * 100;
 
   return (
-    <div className='mb-14 relative'>
+    <div className='mb-10 relative'>
       <div className='px-2 border border-gray-300 bg-white'>
         <div
           className='bg-[#270722] duration-700 rounded-md'
@@ -16,21 +16,9 @@ const ProgressBar = ({ currentStep, totalSteps }) => {
       </div>
 
       <div className='flex justify-between w-full absolute' style={{ top: '-13px' }}>
-        <div style={{
-          borderRadius: '50%',
-          width: '37px',
-          height: '37px'
-        }} className='bg-[#270722] text-white flex justify-center content-center transition-all'>1</div>
-        <div style={{
-          borderRadius: '50%',
-          width: '37px',
-          height: '37px'
-        }} className={`${currentStep >= 2 ? 'bg-[#270722] text-white' : 'bg-[#fff]'} border border-gray-300 flex justify-center content-center duration-1000`}>2</div>
-        <div style={{
-          borderRadius: '50%',
-          width: '37px',
-          height: '37px'
-        }} className={`${currentStep >= 3 ? 'bg-[#270722] text-white' : 'bg-[#fff]'} border border-gray-300 flex justify-center content-center duration-1000`}>3</div>
+        <div className='bg-[#270722] text-white border border-gray-300 rounded-full w-9 h-9 flex items-center justify-center duration-1000'><p>1</p></div>
+        <div className={`${currentStep >= 2 ? 'bg-[#270722] text-white' : 'bg-[#fff]'} border border-gray-300 rounded-full w-9 h-9 flex items-center justify-center duration-1000`}><p>2</p></div>
+        <div className={`${currentStep >= 3 ? 'bg-[#270722] text-white' : 'bg-[#fff]'} border border-gray-300 rounded-full w-9 h-9 flex items-center justify-center duration-1000`}><p>3</p></div>
       </div>
 
     </div>
