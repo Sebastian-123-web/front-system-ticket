863c49ed2e2630790d37e4a46607dff65158a656
Sebastian-123-web <sebastian09_26@hotmail.com>
2023-07-10 17:27:16 -0500

    RB | avanzando la funcion Autenticacion

--- src/helpers/Autenticacion.js
+++ src/helpers/Autenticacion.js
@@ -1,5 +1,8 @@
 import { dataUser } from "../data/dataUsuario"
 
 export const Autenticacion = async (mailUser) => {
-    
+    const dataUser = {
+        user: "user",
+        mail: "mail@mail.com"
+    }
 }
