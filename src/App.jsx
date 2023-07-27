08ad5d4610cd00c2ad47ccc5397e808366b6be59
Sebastian-123-web <sebastian09_26@hotmail.com>
2023-07-27 02:10:41 -0500

    RB | Error 404 corregido, se uso HashRouter y luego npm run build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, HashRouter } from 'react-router-dom';
 
 import DashboardUser from "./page/DashboardUser/DashboardUser";
 import CreateTicket from "./page/DashboardUser/CreateTicket/CreateTicket";
@@ -12,16 +12,16 @@ export default function App() {
 
   return (
     <LoginContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={ <RouterLogin url="/dashboard" > <LoginUser /> </RouterLogin > } />
-          <Route path='dashboard' element={ <RouterDashboard url="/" > <DashboardUser /> </RouterDashboard > } >
-            <Route index element={ <MisTicket /> } />
-            <Route path='/dashboard/createticket' element={ <CreateTicket /> } />
-          </Route>
-          <Route path='*' element={ <h1>Pagina no encontrada :c</h1> } />
-        </Routes>
-      </BrowserRouter>
+      <HashRouter>
+          <Routes>
+            <Route index element={ <RouterLogin url="/dashboard" > <LoginUser /> </RouterLogin > } />
+            <Route path='/dashboard' element={ <RouterDashboard url="/" > <DashboardUser /> </RouterDashboard > } >
+              <Route index element={ <MisTicket /> } />
+              <Route path='/dashboard/createticket' element={ <CreateTicket /> } />
+            </Route>
+            <Route path='*' element={ <h1>Pagina no encontrada :c</h1> } />
+          </Routes>
+      </HashRouter>
     </LoginContextProvider>
   )
 }
