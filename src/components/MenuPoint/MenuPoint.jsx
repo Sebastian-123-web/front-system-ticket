9b38443d8aeda3dadf5104c5e3057ab2582e92ef
Sebastian-123-web <sebastian09_26@hotmail.com>
2023-08-27 02:59:17 -0500

    RB | Se creo el menu para cada ticket(aun falta ordenar mejor para mayor optimizacion)

--- src/components/MenuPoint/MenuPoint.jsx
+++ src/components/MenuPoint/MenuPoint.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react"
+
 export const MenuPoint = () => {
+
+    const [view, setView] = useState(false)
+
+    const handleViewMenu = () => {
+        if(!view){
+            return setView(true)
+        }
+        setView(false)
+    }
+
     return(
-        <div className="cursor-pointer w-10 h-10 rounded-lg hover:bg-slate-200 flex flex-col justify-center items-center" onClick={() => alert('Menu de puntos')}>
-            <div className="w-1 h-1 rounded-lg bg-black mb-1"></div>
-            <div className="w-1 h-1 rounded-lg bg-black mb-1"></div>
-            <div className="w-1 h-1 rounded-lg bg-black"></div>
+        <div className="relative">
+            <div 
+                className="cursor-pointer w-10 h-10 rounded-lg hover:bg-slate-200 flex flex-col justify-center items-center"
+                onClick={() => handleViewMenu() }
+                >
+                    <div className="w-1 h-1 rounded-lg bg-black mb-1"></div>
+                    <div className="w-1 h-1 rounded-lg bg-black mb-1"></div>
+                    <div className="w-1 h-1 rounded-lg bg-black"></div>
+            </div>
+
+
+            <div className={`z-50 w-44 p-2 right-5 bg-[#FAFAFA] rounded-l-lg rounded-br-lg border border-slate-100 shadow-lg shadow-slate-400 absolute ${ view ? ' ' : 'hidden' }`}>
+                <ul>
+                    <li className=" hover:bg-slate-400 hover:text-white hover:font-bold rounded-lg p-2 cursor-pointer">Información</li>
+                    <li className=" hover:bg-slate-400 hover:text-white hover:font-bold rounded-lg p-2 cursor-pointer">Descargar Informe</li>
+                </ul>
+            </div>
         </div>
     )
 }
--- src/page/DashboardUser/MisTicket/MisTicket.jsx
+++ src/page/DashboardUser/MisTicket/MisTicket.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react"
 import { MenuPoint } from "../../../components/MenuPoint/MenuPoint"
 
 export const MisTicket = () => {
+
     return (
         <div className="w-full h-full">
             <div className="mb-5">
@@ -8,9 +10,42 @@ export const MisTicket = () => {
                 <input type="text" className="text-xs" placeholder="Buscar"/>
             </div>
             <div>
-                <table className="w-full">
+                <div className="flex flex-row justify-between items-center mb-2 hover:bg-slate-100 p-2 rounded-lg">
+                    <div className="mr-3 w-[56px]">
+                        <div className="rounded-full w-11 h-11 border-[#EEEEEE] border border-solid flex items-center justify-center">
+                            <p className="text-2xl">
+                                <ion-icon name="ticket-outline"></ion-icon>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="mr-3 w-[45%] min-w-[245px]">
+                        <p className="font-bold">Error SIDIGE</p>
+                        <p>Al encender el equipo muestra ...</p>
+                    </div>
+                    <div className="mr-3 min-w-[125px]">
+                        <div className="inline-flex items-center justify-center bg-[#05668D]/[.6] rounded-2xl p-2">
+                            <div className="w-2.5 h-2.5 rounded-full bg-[#05668D] mr-2"></div>
+                            En Proceso
+                        </div>
+                    </div>
+                    <div className="mr-3 min-w-[87px]">
+                        <div className="flex flex-row items-center justify-center">
+                            <div className="w-2.5 h-2.5 rounded-full bg-red-800 mr-2"></div>
+                            Urgente
+                        </div>
+                    </div>
+                    <div className="mr-3 min-w-[48px]">
+                        <p className="text-4xl">
+                            <ion-icon name="desktop-outline"></ion-icon>
+                        </p>
+                    </div>
+                    <div className="w-[40px]">
+                        <MenuPoint />
+                    </div>
+                </div>
+                {/* <table className="w-full">
                     <tbody>
-                        <tr>
+                        <tr className=" hover:bg-slate-200">
                             <td className="w-14">
                                 <div className="rounded-full w-11 h-11 border-[#EEEEEE] border border-solid flex items-center justify-center">
                                     <p className="text-2xl">
@@ -44,7 +79,7 @@ export const MisTicket = () => {
                             </td>
                         </tr>
                     </tbody>
-                </table>
+                </table> */}
             </div>
         </div>
     )
