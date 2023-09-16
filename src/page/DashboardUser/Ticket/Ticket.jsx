1dcacc68c163914493566c3168d20d7db026ea51
Sebastian-123-web <sebastian09_26@hotmail.com>
2023-09-16 00:48:34 -0500

    RB | Se esta editando la informacion de ticket

--- src/page/DashboardUser/Ticket/Ticket.jsx
+++ src/page/DashboardUser/Ticket/Ticket.jsx
@@ -2,21 +2,27 @@ import { Link } from "react-router-dom"
 
 export const Ticket = () => {
     return (
-        <div className="flex flex-row w-full">
-            <div>
-                <div className="mb-5">
+        <div className="w-full h-full grid grid-cols-12 grid-rows-6 gap-6">
+            <div className="col-span-9 row-span-1">
+                <div className="">
                     <Link to={-1}><ion-icon name="arrow-back-outline"></ion-icon> Atras</Link>
                 </div>
+            </div>
+
+            <div className="col-span-3 row-span-2 bg-white rounded-lg border border-[#E0EDFF] p-5">Noticias</div>
+
+            <div className="col-span-9 row-span-1 bg-white rounded-lg border border-[#E0EDFF] p-5">
                 <div>
-                    <div className="bg-white p-5 rounded-lg border border-[#E0EDFF]" style={{width: "calc(100% - 308px)"}}>
+                    <div>
                         gaaaaaaaaaaaaaaaaaaaaaa
                     </div>
                 </div>
-                <div>
-
-                </div>
             </div>
-            <div>col 2</div>
+
+            <div className="col-span-5 row-span-2 bg-white rounded-lg border border-[#E0EDFF] p-5">Ticket N° 12</div>
+            <div className="col-span-4 row-span-4 bg-white rounded-lg border border-[#E0EDFF] p-5">Descripcion</div>
+            <div className="col-span-3 row-span-4 bg-white rounded-lg border border-[#E0EDFF] p-5">Chat</div>
+            <div className="col-span-5 row-span-2 bg-white rounded-lg border border-[#E0EDFF] p-5">imagenes</div>
         </div>
     )
 }
